3a5093410d2230b4a01c07793b5711b2e3a35f63
aruns2511 <arun.me19@bitsathy.ac.in>
2022-03-05 12:11:19 +0530

    profile photo updated in the page

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -9,9 +9,11 @@ import "./Sidebar.css";
 import SidebarChat from "./SidebarChat";
 import db from './firebase';
 import { useState } from 'react';
+import { useStateValue } from './StateProvider';
 
 function Sidebar() {
     const [rooms,setRooms]=useState([]);
+    const [{user},dispatch]=useStateValue();
 
     useEffect(()=>{
      const unsubscribe=db.collection("rooms").onSnapshot((snapshot)=>
@@ -30,7 +32,7 @@ function Sidebar() {
   return (
     <div className="sidebar">
         <div className="sidebar__header">
-          <Avatar />
+          <Avatar src={user?.photoURL}/>
          <div className='sidebar__headerRight'>
           <IconButton>
             <DonutLarge/>
